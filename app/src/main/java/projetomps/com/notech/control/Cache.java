3af1e74067190413e76a8b0b920e7a0e0ad4cfbe
alef <alef_berg@hotmail.com.br>
2018-06-19 17:26:21 -0300

    DAO + SINGLETON

---
+++ app/src/main/java/projetomps/com/notech/control/Cache.java
@@ -0,0 +1,29 @@
+package projetomps.com.notech.control;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import projetomps.com.notech.model.Comentario;
+import projetomps.com.notech.model.Usuario;
+
+//SINGLETON
+@Database(entities = {Comentario.class, Usuario.class}, version = 1)
+public abstract class Cache extends RoomDatabase {
+    private static final String DB_NAME = "cache.db";
+    private static volatile Cache instancia;
+
+    static synchronized Cache getInstancia(Context context){
+        if(instancia == null)
+            instancia = cria(context);
+        return instancia;
+    }
+
+    private static Cache cria(final Context context) {
+        return Room.databaseBuilder(context, Cache.class, DB_NAME).build();
+    }
+
+    public abstract UsuarioDAO getUsuarioDao();
+    public abstract ComentarioDAO getComentarioDao();
+}
--- app/src/main/java/projetomps/com/notech/control/ComentarioDAO.java
+++ app/src/main/java/projetomps/com/notech/control/ComentarioDAO.java
@@ -2,8 +2,10 @@ package projetomps.com.notech.control;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
 
 import java.util.List;
 
@@ -13,6 +15,12 @@ import projetomps.com.notech.model.Comentario;
 public interface ComentarioDAO {
     @Insert
     void insert(Comentario comentario);
+
+    @Update
+    void update(Comentario comentario);
+
+    @Delete
+    void delete(Comentario comentario);
 
     @Query("SELECT * FROM comentario_table WHERE comentario_table.id = comentario_table.respostaId")
     LiveData<List<Comentario>> getRespostas();
